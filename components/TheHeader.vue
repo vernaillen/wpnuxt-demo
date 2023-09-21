654f53c96c87cfb8e19bd97b70ed6ec5a3fa2ebe
Wouter Vernaillen <wouter@vernaillen.com>
2023-09-21 14:39:52 +0200

    Nuxt+WordPress

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,7 +3,7 @@
         <div class="w-full container mx-auto">
             <div class="grid grid-cols-3">
                 <div class="text-left pt-5 px-0">
-                    <h1 className="text-4xl">Nuxt + WordPress</h1>
+                    <h1><NuxtLink to="/">Nuxt+WordPress</NuxtLink></h1>
                 </div>
                 <div class="col-span-2 text-right py-5 mx-0 px-0 align-top">
                     <ColorModeSwitcher class="mr-4" />
