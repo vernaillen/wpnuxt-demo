21c355fe849898c530e5322c59eceefc2043b90c
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-25 12:15:11 +0200

    show how the composables can be used

--- pages/composables.vue
+++ pages/composables.vue
@@ -1,50 +1,68 @@
 <script setup lang="ts">
+import type { GeneralSettings, Page, Post } from '#graphql-operations';
 import VueJsonPretty from 'vue-json-pretty';
-const userName = await getCurrentUserName()
+
+const settings: GeneralSettings = await useGeneralSettings()
+const userName: string = await getCurrentUserName()
+const pages: Page[] = await usePages()
+const posts: Post[] = await usePosts()
+const staging = await isStaging()
 
 const items = [{
   label: 'useWPUri()',
   content:  useWPUri()
 }, {
-  label: 'await useGeneralSettings()',
-  content: await useGeneralSettings()
+  label: 'const settings: GeneralSettings = await useGeneralSettings()',
+  content: settings
 }, {
-  label: 'await getCurrentUserName()',
+  label: 'const userName: string = await getCurrentUserName()',
   content: userName
 }, {
-  label: 'await usePages()',
-  content: await usePages()
+  label: 'const pages: Page[] = await usePages()',
+  content: pages
 }, {
-  label: 'await usePosts()',
-  content: await usePosts()
+  label: 'const posts: Post[] = await usePosts()',
+  content: posts
 }, {
-  label: 'await isStaging()',
-  content: await isStaging()
+  label: 'const staging = await isStaging()',
+  content: staging
 }]
 </script>
 
 <template>
-    <UMain>
-        <UContainer class="prose dark:prose-invert mt-10">
-            <h2>Authentication</h2>
-            <div v-if="userName">
-                Authenticated in WordPress as {{ userName }}<br>
-                <a href="/logout">Log out</a>
-            </div>
-            <div v-else>
-                not logged in<br>
-                <a href="/login?redirect=/test">Log in</a>
-            </div>
-            <h2>Composables</h2>
-            <UAccordion :items="items">
-                <template #item="{ item }">
-                  <pre class="mt-0 py-0">
-                    <code class="flex justify-start py-0">
-                        <vue-json-pretty :path="'res'" :data="item.content"> </vue-json-pretty>
-                    </code>
-                  </pre>
-                </template>
-            </UAccordion>
-        </UContainer>
-    </UMain>
+  <UMain>
+    <UContainer class="prose dark:prose-invert mt-10">
+        <h2>Authentication</h2>
+        <div v-if="userName">
+            Authenticated in WordPress as {{ userName }}<br>
+            <a href="/logout">Log out</a>
+        </div>
+        <div v-else>
+            not logged in<br>
+            <a href="/login?redirect=/test">Log in</a>
+        </div>
+        <h2>Composables</h2>
+        <div class="relative">
+          <UAccordion 
+            :items="items"
+            :ui="{
+              default: {
+                class: 'pl-8 mb-3',
+              },
+              item: {
+                icon: 'absolute left-1 transform transition-transform duration-200',
+              },
+            }"
+          >
+              <template #item="{ item }">
+                <pre class="mt-0 py-0">
+                  <code class="flex justify-start py-0">
+                      <vue-json-pretty :path="'res'" :data="item.content"> </vue-json-pretty>
+                  </code>
+                </pre>
+              </template>
+          </UAccordion>
+        </div>
+    </UContainer>
+  </UMain>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,7 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
   content: [
     './node_modules/@vernaillen/wpnuxt/dist/runtime/components/**/*.vue',
   ],
-  "theme": {
-    fontFamily: {
-      sans: [...defaultTheme.fontFamily.sans],
-      serif: [...defaultTheme.fontFamily.serif],
-    },
-    "extend": {
-      "colors": {
-        "wpgray": {
-          DEFAULT: "#32373c",
-          "50": "#ebebec",
-          "100": "#d6d7d8",
-          "200": "#adafb1",
-          "300": "#84878a",
-          "400": "#5b5f63",
-          "500": "#32373c",
-          "600": "#282c30",
-          "700": "#1e2124",
-          "800": "#141618",
-          "900": "#0a0b0c"
-        }
-      },
-    }
-  }
 }
