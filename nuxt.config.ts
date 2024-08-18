076cc1acb6cb5aa1a883642f3f4ecdebd0709fed
Wouter Vernaillen <wouter@vernaillen.com>
2024-08-18 11:34:23 +0200

    use wordpress @ WPEngine & configure  prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,17 +18,13 @@ export default defineNuxtConfig({
   },
 
   wpNuxt: {
-    wordpressUrl: 'https://wordpress.wpnuxt.com',
+    wordpressUrl: 'https://wpnuxtdev.wpenginepowered.com',
     frontendUrl: 'https://demo.wpnuxt.com',
     defaultMenuName: 'main',
-    blocks: true,
     enableCache: true,
     staging: false,
     logLevel: 4,
-    downloadSchema: true,
-    generateComposables: {
-      enabled: true
-    }
+    downloadSchema: true
   },
 
   eslint: {
@@ -66,6 +62,24 @@ export default defineNuxtConfig({
   routeRules: {
     '/**': {
       prerender: true
+    }
+  },
+
+  /**
+   * Depending on your servers capabilities, you may need to adjust the following settings.
+   * It will affect the build time but also increase the reliability of the build process.
+   * If you have a server with a lot of memory and CPU, you can remove the following settings.
+   * @property {number} concurrency - How many pages to prerender at once
+   * @property {number} interval - How long to wait between prerendering pages
+   * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated
+   */
+  nitro: {
+    prerender: {
+      concurrency: 10,
+      interval: 1000,
+      failOnError: false,
+      autoSubfolderIndex: false,
+      routes: process.env.WPNUXT_STAGING ? [] : ['/', '/sample-page', '/about', '/composables', '/generated-composables', '/test-image-gallery', '/nog-een-post-in-wordpress', 'test-wp-nuxt', 'hello-world']
     }
   },
 
