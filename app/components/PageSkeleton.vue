2f1b25b566237088b51f6357f3cc4e26230f9bd1
Wouter Vernaillen <wouter@vernaillen.com>
2024-08-19 22:53:36 +0200

    PageSkeleton

---
+++ app/components/PageSkeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <UPage>
+    <UPageHeader>
+      <UIcon
+        name="i-svg-spinners-bars-fade"
+        class="mt-[18px] opacity-30"
+      />
+    </UPageHeader>
+    <div class="mt-9 space-y-3">
+      <USkeleton class="h-3 w-[200px] mb-7" />
+      <USkeleton class="h-4 w-2/3" />
+      <USkeleton class="h-4 w-1/2" />
+    </div>
+    <template #left>
+      <UAside>
+        <PrevNext
+          :prev="undefined"
+          :next="undefined"
+          class="mt-2 mb-12"
+        />
+      </UAside>
+    </template>
+  </UPage>
+</template>
